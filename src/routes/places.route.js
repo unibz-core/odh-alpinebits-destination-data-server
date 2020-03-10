c514c1933cdbcdc57a555fe256eaf9a21c4dcac2
Tiago Prince Sales <tiago.princesales@unibz.it>
2020-03-10 18:12:33 +0100

    Fixed routes and base endpoint response

--- src/routes/places.route.js
+++ src/routes/places.route.js
@@ -1,11 +1,11 @@
 const errors = require('../errors');
 
 module.exports = function(app) {
-  app.get('/api/1.0/places', function(req, res) {
+  app.get('/1.0/places', function(req, res) {
     errors.handleNotImplemented(req,res);
   });
 
-  app.get('/api/1.0/places/:id', function(req, res) {
+  app.get('/1.0/places/:id', function(req, res) {
     errors.handleNotImplemented(req,res);
   });
 }
